fb1e58151d9319932d88cfc558b8e4001f6dd19c
Jon Tapanes <jon@Jons-MacBook-Air.local>
2018-09-14 10:29:59 -0400

    routing?

--- server.js
+++ server.js
@@ -5,6 +5,23 @@ var http = require("http");
 
 // Define a port to listen for incoming requests
 var PORT = 8080;
+var app = express();
+
+app.use(express.static(path.join(__dirname, './app/public')));
+
+// Add middleware for parsing incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Start listening on PORT
+app.listen(PORT, function() {
+  console.log('Friend Finder app is listening on PORT: ' + PORT);
+});
 
 // Create a generic function to handle requests and responses
 function handleRequest(request, response) {
@@ -23,3 +40,5 @@ server.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
+
